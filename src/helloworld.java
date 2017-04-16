f84b4c040df9f098e5721205486440f2c8bb1d77
annjt <hanghoang.ann@gmail.com>
2017-04-17 00:22:41 +0700

    implement div func

--- src/helloworld.java
+++ src/helloworld.java
@@ -25,4 +25,8 @@ public class helloworld {
         int result = x + y;
         return result;
     }
+    public static int div(int x, int y){
+        int result = x/y;
+        return result;
+    }
 }
